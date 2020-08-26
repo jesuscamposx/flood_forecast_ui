d43887fd42a2646cecb2e68850d91e35a42e4cda
jcampos <jcampos.jc38@gmail.com>
2020-08-25 23:54:06 -0500

    Add base url for floods file service

--- src/services/ServicesEndpoints.js
+++ src/services/ServicesEndpoints.js
@@ -1,10 +1,12 @@
+const baseUrl = process.env.REACT_APP_API_URL;
+
 const SERVICE_ENDPOINTS = {
     LIST_SENSORS: '/api/sensor',
     LIST_MEASURES: '/api/medicion',
     GET_LAST_MEASURE: '/api/medicion/ultima?sensor=',
     GET_FLOOD_CATALOGUE: '/api/archivo/inundacion/catalogo',
     GET_MEASURES_CATALOGUE: '/api/archivo/nivel-agua/catalogo',
-    GET_FLOOD_FILE: '/api/archivo/inundacion?years=',
+    GET_FLOOD_FILE: baseUrl + '/api/archivo/inundacion?years=',
     GET_MEASURES_FILE: '/api/archivo/nivel-agua?years=',
     LIST_TOWNS: '/api/alcaldia',
     LIST_QUARTERS: '/api/colonia',
