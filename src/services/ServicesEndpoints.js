9e0896ff19c850feb926e6c80ecb53367e6ffec8
jcampos <jcampos.jc38@gmail.com>
2020-08-26 09:30:27 -0500

    Modify link type in menu

--- src/services/ServicesEndpoints.js
+++ src/services/ServicesEndpoints.js
@@ -7,7 +7,7 @@ const SERVICE_ENDPOINTS = {
     GET_FLOOD_CATALOGUE: '/api/archivo/inundacion/catalogo',
     GET_MEASURES_CATALOGUE: '/api/archivo/nivel-agua/catalogo',
     GET_FLOOD_FILE: baseUrl + '/api/archivo/inundacion?years=',
-    GET_MEASURES_FILE: '/api/archivo/nivel-agua?years=',
+    GET_MEASURES_FILE: baseUrl + '/api/archivo/nivel-agua?years=',
     LIST_TOWNS: '/api/alcaldia',
     LIST_QUARTERS: '/api/colonia',
     LIST_STREETS: '/api/calle?colonia=',
